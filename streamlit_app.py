42f8431b1729e7029f3e5be3ca6c9badcc385109
jytbrd <156661538+jytbrd@users.noreply.github.com>
2024-01-15 18:48:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,18 @@
 import streamlit as st
 
-# Initialize connection.
+from snowflake.snowpark import Session
 
-conn = st.connection(**st.secrets["connections.snowflake"])
+from secrets import SNOWFLAKE_ACCOUNT, SNOWFLAKE_USER, SNOWFLAKE_PASSWORD
+
+session = Session(url=st.secrets["SNOWFLAKE_ACCOUNT"], user=st.secrets["SNOWFLAKE_USER"], password=st.secrets["SNOWFLAKE_PASSWORD"])
+
+df = session.sql("select * from JAMF_DEVICE_INFO.PUBLIC.WATCHTOWER_STATIONS_TBL").to_pandas()
+
+st.write(df)
 
 # Perform query.
-df = conn.query("SELECT * from WATCHTOWER_STATIONS_TBL;", ttl=600)
+#df = conn.query("SELECT * from WATCHTOWER_STATIONS_TBL;", ttl=600)
 
 # Print results.
-for row in df.itertuples():
-    st.write(f"{row.ID} has a :{row.ORGANIZATIONUNITID}:")
+#for row in df.itertuples():
+    #st.write(f"{row.ID} has a :{row.ORGANIZATIONUNITID}:")
