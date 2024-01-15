f68fe9c1aa43891ae19bebe01eaf66da84202cfd
jytbrd <156661538+jytbrd@users.noreply.github.com>
2024-01-15 18:35:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,11 @@
-# Snowpark
-from snowflake.snowpark.session import Session
-from snowflake.snowpark.functions import avg, sum, col,lit
 import streamlit as st
-import pandas as pd
 
-def create_session_object():
-    connection_parameters = st.secrets["snowflake"]
-    session = Session.builder.configs(connection_parameters).create()
-    return session
+# Initialize connection.
+conn = st.connection("snowflake")
 
-  
-# Create Snowpark DataFrames that loads data from Knoema: Environmental Data Atlas
-def load_data(session):
-    snow_df_co2 = session.table("SNOWFLAKE.ACCOUNT_USAGE.WAREHOUSE_METERING_HISTORY")\
-                         .select(col("WAREHOUSE_NAME"),col("CREDITS_USED")).collect()
-    
-    # Convert Snowpark DataFrames to Pandas DataFrames for Streamlit
-    pd_df_co2  = pd.DataFrame(snow_df_co2)
-    
-    st.write(pd_df_co2)
-    
+# Perform query.
+df = conn.query("SELECT * from WATCHTOWER_STATIONS_TBL;", ttl=600)
 
-if __name__ == "__main__":
-    session = create_session_object()
-    load_data(session)
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.ID} has a :{row.ORGANIZATIONUNITID}:")
